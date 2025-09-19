627afe3dc338897ed7d45ad6aa723eee5cfdb24d
uvesh <uq588948@gmail.com>
2025-09-19 21:51:47 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from tinymce.models import HTMLField
+from autoslug import AutoSlugField
+
+class News(models.Model):
+    news_title=models.CharField(max_length=100)
+    news_desc=HTMLField()
+    
+    
+    
+    
+    news_slug=AutoSlugField(populate_from='news_title',unique=True,null=True,default=None)
+    
+    
+    
+    
+    
+    
+    # passsword:uvesh123
+
